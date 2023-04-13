7b27bfaa2810669d0e75e85e5e75d0a6b4f3d36c
Sanjit Sarkar <sanjit.est14@gmail.com>
2023-04-13 15:45:58 +0600

    N_Char

---
+++ N_Char.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    char a;
+    scanf("%c", &a);
+    if(a>='a'&& a<='z'){
+        int res = a - 32;
+        printf("%c", res);
+    }else{
+        int res = a + 32;
+        printf("%c", res);
+    }
+        
+    return 0;
+}
