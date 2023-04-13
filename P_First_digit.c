052b948fe6e284ebdafd0d9459247ac8b0f3e5fc
Sanjit Sarkar <sanjit.est14@gmail.com>
2023-04-13 16:30:23 +0600

    P_First_digit

---
+++ P_First_digit.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int x;
+    scanf("%d", &x);
+    int res = x / 1000;
+    if(res%2==0){
+        printf("EVEN");
+    }else{
+        printf("ODD");
+    }
+    return 0;
+}
